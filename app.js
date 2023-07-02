0db036d487c1e16f8af19205fa283ed347a0f30a
Nitesh Pandey <nitesh.pandey@kalgudi.com>
2023-07-02 20:49:43 +0530

    fix for CORS

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.contentType('json');
+  return next();
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -35,7 +42,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
