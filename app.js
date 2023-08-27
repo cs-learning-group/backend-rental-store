a3bfd9612185ea833bc9b2d199cf585e7c2066f4
Coaxtron Roaxtron <coaxtron@Coaxtrons-MacBook-Air.local>
2023-08-27 16:52:08 +0530

    Product API List

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/product');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 })
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
